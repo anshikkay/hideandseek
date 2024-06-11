b47a26f690bbb66a2e4699e5ff9b22c67b0f543b
Anshika <anshikaa1711@gmail.com>
2024-06-11 15:36:17 +0530

    Add existing file

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Routes,Route} from 'react-router-dom';
+import './globals.css';
+const App = () => {
+  return (
+    <main className="flex h-screen">
+        <Routes>
+            <Route path="sign-in" element={<SignInForm/>}/>
+            <Route index element={<Home/>}/>
+        </Routes>
+    </main>
+  )
+}
+
+export default App
